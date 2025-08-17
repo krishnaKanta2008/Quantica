8d79be2b85b2925f3f273b724ab51881dd4f4afa
krishnaKanta2008 <kb0397206@gmail.com>
2025-08-17 20:12:28 +0530

    Refactor code structure for improved readability and maintainability

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -62,7 +62,7 @@ const Footer = () => {
                   <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                   <div className="sm:flex sm:items-center sm:justify-between">
                       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                          © 2024{" "}
+                          © 2025{" "}
                           <a href="/" className="hover:underline">
                               Quantica
                           </a>
